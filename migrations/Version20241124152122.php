8ed3791a75d44ef4059bfa763fa085473a0e6f49
Ryze <rrll20220@gmail.com>
2024-11-24 16:23:06 +0100

    first commit

---
+++ migrations/Version20241124152122.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241124152122 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE url (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, original_url VARCHAR(2083) NOT NULL, short_url VARCHAR(5) NOT NULL, date DATETIME NOT NULL, INDEX IDX_F47645AEA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE url ADD CONSTRAINT FK_F47645AEA76ED395 FOREIGN KEY (user_id) REFERENCES `user` (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE url DROP FOREIGN KEY FK_F47645AEA76ED395');
+        $this->addSql('DROP TABLE url');
+    }
+}
---
+++ src/Entity/Url.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UrlRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UrlRepository::class)]
+class Url
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 2083)]
+    private ?string $originalUrl = null;
+
+    #[ORM\Column(length: 5)]
+    private ?string $shortUrl = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $date = null;
+
+    #[ORM\ManyToOne(inversedBy: 'urls')]
+    private ?User $user = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getOriginalUrl(): ?string
+    {
+        return $this->originalUrl;
+    }
+
+    public function setOriginalUrl(string $originalUrl): static
+    {
+        $this->originalUrl = $originalUrl;
+
+        return $this;
+    }
+
+    public function getShortUrl(): ?string
+    {
+        return $this->shortUrl;
+    }
+
+    public function setShortUrl(string $shortUrl): static
+    {
+        $this->shortUrl = $shortUrl;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): static
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): static
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+}
